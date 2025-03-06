5989d5c73c82ca1197685e3ddc302307524b8cb3
Mustafa Çömez <m.comez3437@gmail.com>
2025-03-06 17:10:41 +0300

    Proje dosyası ekle.

---
+++ ApiProject.WebApi/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiProject.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= DESKTOP-NDJBGQC; initial catalog=ApiYummyDb; integrated security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
