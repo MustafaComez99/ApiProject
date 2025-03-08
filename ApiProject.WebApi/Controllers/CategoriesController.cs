c19db0e82259dcbe586341fa90881da336999541
Mustafa Çömez <m.comez3437@gmail.com>
2025-03-08 03:11:58 +0300

    .

---
+++ ApiProject.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using ApiProject.WebApi.Context;
+using ApiProject.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        //Listeleme
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        //Silme
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme İşlemi Tamamlandı.");
+        }
+
+        //ID'ye Göre Getirme
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        //Güncelleme
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncelleme İşlemi Başarılı.");
+        }
+
+        //Ekleme
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+    }
+}
